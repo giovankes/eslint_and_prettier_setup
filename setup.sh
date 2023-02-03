f043eb032c633f8d90d698635ac3dc005c4ae748
giovankes <hello@giotje.dev>
2023-02-03 13:38:35 +0100

    Revert "fixed bun command"
    This reverts commit 53e6ae7997be6d4307c5ea6f4e9536f280a0a13a.

--- setup.sh
+++ setup.sh
@@ -100,30 +100,18 @@ echo -e "${GREEN}Configuring your development environment... ${NC}"
 echo
 echo -e "1/5 ${LCYAN}ESLint & Prettier Installation... ${NC}"
 echo
-if [$pkg_cmd == "bun"]; then 
-  $pkg_cmd eslint prettier 
-else
-  $pkg_cmd -D eslint prettier
-fi
+$pkg_cmd -D eslint prettier
 
 echo
 echo -e "2/5 ${YELLOW}Conforming to Airbnb's JavaScript Style Guide... ${NC}"
 echo
-if [$pkg_cmd == 'bun'];then 
-$pkg_cmd  eslint-config-airbnb eslint-plugin-jsx-a11y eslint-plugin-import eslint-plugin-react  @babel/eslint-parser babel-preset-airbnb @babel/core @babel/plugin-transform-runtime
-else
 $pkg_cmd -D eslint-config-airbnb eslint-plugin-jsx-a11y eslint-plugin-import eslint-plugin-react  @babel/eslint-parser babel-preset-airbnb @babel/core @babel/plugin-transform-runtime
-fi
 
 echo
 echo -e "3/5 ${LCYAN}Making ESlint and Prettier play nice with each other... ${NC}"
 echo "See https://github.com/prettier/eslint-config-prettier for more details."
 echo
-if [$pkg_cmd == 'bun']; then
-$pkg_cmd  eslint-config-prettier eslint-plugin-prettier eslint-plugin-react-hooks
-else
 $pkg_cmd -D eslint-config-prettier eslint-plugin-prettier eslint-plugin-react-hooks
-fi
 
 if [ "$skip_eslint_setup" == "true" ]; then
   break
